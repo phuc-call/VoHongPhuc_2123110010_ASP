4610ff9d3106fe4a5e9226a02e8504dadb6e69cf
Vo Phuc <phucboygo@gmail.com>
2026-05-28 00:43:12 +0700

    Buoi2: Thêm UserController hiển thị danh sách thành viên

--- CMS.Backend/Controllers/UserController.cs
+++ CMS.Backend/Controllers/UserController.cs
@@ -1,41 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using CMS.Data.Entities;
+using CMS.Data;
 
 namespace CMS.Backend.Controllers
 {
     public class UserController : Controller
     {
+        private readonly ApplicationDbContext _context;
         // Hàm Index: Hiển thị danh sách thành viên quản trị
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            // 1. Tạo danh sách Người dùng giả (Mock Data)
-            var users = new List<User>
-            {
-                new User
-                {
-                    Id = 1,
-                    Username = "admin_thai",
-                    FullName = "Nguyễn Cao Thái",
-                    Role = "Administrator"
-                },
-                new User
-                {
-                    Id = 2,
-                    Username = "editor_01",
-                    FullName = "Trần Văn Biên Tập",
-                    Role = "Editor"
-                },
-                new User
-                {
-                    Id = 3,
-                    Username = "author_minh",
-                    FullName = "Lê Quang Minh",
-                    Role = "Author"
-                }
-            };
-
-            // 2. Trả về View kèm theo danh sách người dùng
-            return View(users);
+            var posts = _context.Users.ToList();
+            return View(posts);
         }
     }
 }
---
+++ CMS.Data/ApplicationDbContext.csClass1.cs
@@ -0,0 +1,27 @@
+﻿using CMS.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        // Khai báo các bảng dữ liệu
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<CategoryProduct> CategoriesProducts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+    }
+}
